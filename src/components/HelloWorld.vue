ab5169a389f05538f341c11460327716ce41b7be
Yukilwc <misaka1033@gmail.com>
2021-04-11 14:43:18 +0800

    project start

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="prefixer">prefixer</div>
+  <div class="rem">rem fonts</div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+
+<style scoped lang='scss'>
+.prefixer {
+  transform: translateX(-10px);
+}
+.rem {
+  font-size: 18px;
+  width: 160px;
+}
+</style>
